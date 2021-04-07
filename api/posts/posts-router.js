b024fabbd2c869a8158eefdaa8d8cbc5277ec1df
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-07 14:59:29 -0700

    MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,169 @@
-// implement your posts router here
+// implement your posts router her
+
+const Posts = require('./posts-model.js')
+const express=require("express")
+const router=express.Router()
+
+
+// | 1 | GET    | /api/posts              | Returns **an array of all the post objects** contained in the database                                                                   |
+router.get('/', (req,res)=>{
+    Posts.find()
+    .then(posts=>{
+        res.status(200).json(posts)
+    })
+    .catch(()=>{
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    })
+})
+
+// | 2 | GET    | /api/posts/:id          | Returns **the post object with the specified id**                                                                                        |
+router.get('/:id', (req,res)=>{
+    const {id}=req.params
+    Posts.findById(id)
+    
+    .then(posts=>{
+        if(!posts){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }else{
+            res.status(200).json(posts)
+        }
+    })
+    .catch(()=>{
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    })
+})
+
+// | 3 | POST   | /api/posts              | Creates a post using the information sent inside the request body and returns **the newly created post object**                          |
+router.post('/', (req,res)=>{
+    const newPost = req.body
+    if(!newPost.title || !newPost.contents){
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    }else{
+        Posts.insert(newPost)
+        //code from Gabe's follow-along - works
+            .then(({id})=>{
+                return Posts.findById(id)
+            })
+            .then(posts=>{
+                res.status(201).json(posts)
+            })
+        
+            //my code from homwork, also works.
+        // .then(posts=>{
+        //     const postID = posts.id
+        //     Posts.findById(postID)
+        //     .then(posts=>{
+        //         res.status(201).json(posts)
+        //     })
+        // })
+        .catch(()=>{
+            res.status(500).json({ message: "There was an error while saving the post to the database" })
+        })
+    }       
+})
+
+// | 4 | PUT    | /api/posts/:id          | Updates the post with the specified id using data from the request body and **returns the modified document**, not the original          |
+
+router.put('/:id', async(req,res)=>{
+    const {id} = req.params
+    const changes = req.body
+   
+    //USING TRY/CATCH
+    
+        if(!changes.title || !changes.contents){
+            res.status(400).json({ message: "Please provide title and contents for the post" })
+        }else{
+            
+            Posts.findById(id)
+            .then(stuff=>{
+                if(!stuff){
+                    res.status(404).json({message: "The post with the specified ID does not exist"})
+                }else{
+                    return Posts.update(id, changes)
+                }
+            })
+            .then(data=>{
+                if(data){
+                    return Posts.findById(id)
+                }
+            })
+            .then(post=>{
+                if(post){
+                    res.json(post)
+                }
+            })
+            .catch(err=>{ res.status(500).json({ message: "The post information could not be modified"})
+        
+            })
+
+        }
+    })
+            // const updatedPost = await Posts.update(id, changes)
+            // if(!updatedPost){
+            //     res.status(422).json("User doesn't exist.")
+            // }else{
+            //     res.status(200).json(updatedPost)
+            // }
+        
+
+
+// | 5 | DELETE | /api/posts/:id          | Removes the post with the specified id and returns the **deleted post object**       
+router.delete('/:id', async (req,res)=>{
+    const {id}=req.params
+
+    try{
+        const post = await Posts.findById(id)
+        if(!post){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }else{
+            const numOfDeletedPosts = await Posts.remove(req.params.id) // if this works, it should be one
+            // res.status(200).json({ message: 'The post has been deleted' })
+            res.json(post)
+        }
+    }catch(err){
+        res.status(500).json({ message: "The post could not be removed" })
+    }
+
+    // Posts.remove(id)
+    // .then(posts=>{
+    //     if(posts>0){
+    //         res.status(200).json({ message: 'The post has been deleted' });
+    //     }else {
+    //         res.status(404).json({ message: "The post with the specified ID does not exist" });
+    //       }
+    // })
+    // .catch(()=>{
+    //     res.status(500).json({ message: "The post could not be removed" })
+    // })
+})
+// | 6 | GET    | /api/posts/:id/comments | Returns an **array of all the comment objects** associated with the post with the specified id    
+router.get('/:id/comments', async (req,res)=>{
+    const {id}=req.params.id
+
+    try{
+        const post = await Posts.findById(id)
+        if(!post){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }else{
+            const comments = await Posts.findPostComments(id)
+            res.json(comments)
+        }
+
+    }catch(err){
+        res.status(500).json({ message: "The comments information could not be retrieved" })
+    }
+
+    // //Posts.findbyId
+    // // if post exists(findPost comments)
+    // Posts.findPostComments(postId)
+    // console.log(postId)
+    // .then(postCom=>{
+    //     console.log('test')
+    //     res.status(200).json(postCom)
+    // })
+    // .catch(()=>{
+    //     res.status(500).json({ message: "The comments information could not be retrieved" })
+    // })
+})
+module.exports = router;
+
--- api/server.js
+++ api/server.js
@@ -1,119 +1,25 @@
 // implement your server here
+// require your posts router and connect it here
 //STEP 3:
 const express = require("express")
+const postsRouter= require('./posts/posts-router.js')
 
 
 //STEP 4:
 const server = express();
 
+
 //STEP 5:
 server.use(express.json())
-// require your posts router and connect it here
 
-const Posts = require('./posts/posts-model')
-// | 1 | GET    | /api/posts              | Returns **an array of all the post objects** contained in the database                                                                   |
-server.get('/api/posts', (req,res)=>{
-    Posts.find()
-    .then(posts=>{
-        res.status(200).json(posts)
-    })
-    .catch(err=>{
-        res.status(500).json({message: err.message})
-    })
-})
-// | 2 | GET    | /api/posts/:id          | Returns **the post object with the specified id**                                                                                        |
-server.get('/api/posts/:id', (req,res)=>{
-    const {id}=req.params
-    Posts.findById(id)
-    
-    .then(posts=>{
-        if(!posts){
-            res.status(404).json("Post not found")
-        }else{
-            res.status(200).json(posts)
-        }
-    })
-    .catch(err=>{
-        res.status(500).json({message: err.message})
+server.use('/api/posts', postsRouter)
+
+server.use('*', (req,res)=>{
+    res.status(404).json({
+        message: 'not found'
     })
 })
 
-// | 3 | POST   | /api/posts              | Creates a post using the information sent inside the request body and returns **the newly created post object**                          |
-server.post('/api/posts', (req,res)=>{
-    const newPost = req.body
-    if(!newPost.title || !newPost.contents){
-        res.status(404).json("Title or contents not found. Please add a title and content.")
-    }else{
-        Posts.insert(newPost)
-        .then(posts=>{
-            const postID = posts.id
-            Posts.findById(postID)
-            .then(posts=>{
-                res.status(200).json(posts)
-            })
-        })
-        .catch(err=>{
-            res.status(500).json({message: err.message})
-        })
-    }       
-})
-
-// | 4 | PUT    | /api/posts/:id          | Updates the post with the specified id using data from the request body and **returns the modified document**, not the original          |
-
-server.put('/api/posts/:id', async(req,res)=>{
-    const {id} = req.params
-    const changes = req.body
-   
-    //USING TRY/CATCH
-    // try{
-    //     if(!changes.title || !changes.contents){
-    //         res.status(422).json("Title and content required.")
-    //     }else{
-    //         const updatedPost = await Posts.update(id, changes)
-    //         if(!updatedPost){
-    //             res.status(422).json("User doesn't exist.")
-    //         }else{
-    //             res.status(201).json(updatedPost)
-    //         }
-            
-    //     }
-        
-    // }catch(err){
-    //     res.status(500).json({message: err.message})
-    // }
-
-
-    //USING .THEN and .CATCH
-    Posts.update(id, changes)
-    .then(posts=>{
-        if(posts){
-            res.status(200).json(posts)
-        }else{
-            res.status(404).json("The post cannot be found.")
-        }
-    })
-    .catch(err=>{
-        res.status(500).json({message: err.message})
-    })
-})
-
-// | 5 | DELETE | /api/posts/:id          | Removes the post with the specified id and returns the **deleted post object**       
-server.delete('/api/posts/:id', (req,res)=>{
-    const {id}=req.params
-    Posts.remove(id)
-    .then(posts=>{
-        if(posts>0){
-            res.status(200).json({ message: 'The post has been deleted' });
-        }else {
-            res.status(404).json({ message: 'The post could not be found' });
-          }
-    })
-    .catch(err=>{
-        res.status(500).json({message: err.message})
-    })
-})
-
-// | 6 | GET    | /api/posts/:id/comments | Returns an **array of all the comment objects** associated with the post with the specified id    
 
 
 
